cefe75576f5306e2fd35cb11e32b27956c342adf
Emily Pisaygnane <episaygnane@me.com>
2022-11-30 11:20:14 -0800

    wrote out test for POST in users.test, created route for creating new user

---
+++ __tests__/users.test.js
@@ -0,0 +1,32 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const fakeUser = {
+  email: 'test@test.com',
+  password: '123456'
+};
+
+describe('/api/v1/users routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it.skip('/api/v1/users registers a new user', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send(fakeUser);
+    const { email } = fakeUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email
+    });
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+});
+  
